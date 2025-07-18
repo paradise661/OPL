4e5fe85b71ffbd49331f0f93b76d1813f20aa6b4
Shrayashd <shrayash000@gmail.com>
2025-07-18 10:47:24 +0545

    404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.frontend.master')
 @section('seo')
-    @include('frontend.global.seo', [
-        'name' => '404',
-        'title' => '404',
-        'description' => '404',
-        'keyword' => '404',
-        'schema' => '404',
-        'seoimage' => 'frontend/assets/images/404.png',
-        'created_at' => '2023-06-06T08:09:15+00:00',
-        'updated_at' => '2023-06-06T10:54:05+00:00',
-    ])
-@endsection
 
 @section('content')
     <div class="error-wrapper pt-100">
