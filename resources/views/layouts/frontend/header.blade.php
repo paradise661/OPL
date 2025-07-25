43b3f9127c14756e55ec0040f0d8b599936b7fbd
Shrayashd <shrayash000@gmail.com>
2025-07-25 13:11:50 +0545

    Department done

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -64,62 +64,10 @@
                                         <li><a href="service-details.html">Division</a></li>
                                     </ul>
                                 </li>
-                                {{-- <li class="menu-item-has-children mega-menu-wrap">
-                                    <a href="javascript:void(0)">Department</a>
-                                    <ul class="mega-menu">
-                                        <li><a href="products-grid.html">Page List 1</a>
-                                            <ul>
-                                                <li><a href="index.html">Home 1</a></li>
-                                                <li><a href="index-2.html">Home 2</a></li>
-                                                <li><a href="index-3.html">Home 3</a></li>
-                                                <li><a href="about.html">About</a></li>
-                                                <li><a href="history.html">History</a></li>
-                                                <li><a href="gallery.html">Gallery</a></li>
-                                            </ul>
-                                        </li> --}}
-                                {{-- <li><a href="#">Page List 2</a>
-                                            <ul>
-                                                <li><a href="services.html">Services</a></li>
-                                                <li><a href="service-details.html">Service Details</a></li>
-                                                <li><a href="pricing-plan.html">Pricing Plan</a></li>
-                                                <li><a href="faq.html">Faq's</a></li>
-                                                <li><a href="blog.html">Blog</a></li>
-                                                <li><a href="blog-details.html">Blog Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Page List 3</a>
-                                            <ul>
-                                                <li><a href="products-grid.html">Product Grid</a></li>
-                                                <li><a href="products-grid-sidebar.html">Product Grid Sidebar</a></li>
-                                                <li><a href="products-list.html">Product List</a></li>
-                                                <li><a href="products-list-sidebar.html">Product List Sidebar</a></li>
-                                                <li><a href="shop-details.html">Shop Details</a></li>
-                                                <li><a href="product-details-sidebar.html">Shop Details Sidebar</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Page List 4</a>
-                                            <ul>
-                                                <li><a href="account.html">Account</a></li>
-                                                <li><a href="cart.html">My Cart</a></li>
-                                                <li><a href="checkout.html">Checkout</a></li>
-                                                <li><a href="404.html">404 Error</a></li>
-                                                <li><a href="contact.html">Contact Us</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li> --}}
+
                                 <li>
                                     <a href="/departments">Department</a>
-                                    {{-- <ul class="sub-menu">
-                                        <li><a href="products-grid-sidebar.html">Product Grid Sidebar</a></li>
-                                        <li><a href="products-list.html">Product List</a></li>
-                                        <li><a href="products-list-sidebar.html">Product List Sidebar</a></li>
-                                        <li><a href="shop-details.html">Shop Details</a></li>
-                                        <li><a href="product-details-sidebar.html">Shop Details Sidebar</a></li>
-                                        <li><a href="account.html">Account</a></li>
-                                        <li><a href="cart.html">My Cart</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
-                                    </ul> --}}
+
                                 </li>
                                 <li class="menu-item-has-children">
                                     <a href="products-grid.html">Blogs</a>
@@ -143,29 +91,10 @@
                             </ul>
                         </nav>
                     </div>
-                    {{-- <div class="col-auto d-none d-xl-block">
-                        <form class="header-search">
-                            <button class="searchBoxTggler" aria-label="search-button">
-                                <i><img src="frontend/assets/img/icons/search.png" alt=""></i>
-                            </button>
-                            <input type="text" placeholder="Search">
-                        </form>
-                    </div>
-                    <div class="col-auto">
-                        <div class="header-btns">
-                            <a class="user-btn d-none d-md-inline-flex" href="contact.html">
-                                <img src="frontend/assets/img/icons/user.png" alt="user">
-                            </a>
-                            <a class="sideCartToggler d-none d-md-inline-flex" href="cart.html">
-                                <i class="fas fa-shopping-basket"></i>
-                                <span>0</span>
-                            </a>
-                            <button class="vs-menu-toggle d-inline-flex d-lg-none"><i class="fal fa-bars"></i></button>
-                        </div>
-                    </div> --}}
+
                 </div>
             </div>
         </div>
     </div>
-    <!-- Main Menu Area -->
+
 </header>
